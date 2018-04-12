4920e76359a0dbacde9c9c05ffe0b9b7ed8f2b5e
Unknown <ramirezversion@gmail.com>
2018-04-12 13:37:45 +0200

    Asked for teacher review

--- practica1_variables.sh
+++ practica1_variables.sh
@@ -173,6 +173,7 @@ iptables -A FORWARD -i $IFACE_DMZ -s $IP_DMZ_SERVER -o $IFACE_WAN -d $IP_INET -p
 # pasv_min_port=10090
 # pasv_max_port=10100
 iptables -A FORWARD -i $IFACE_WAN -s $IP_INET -o $IFACE_DMZ -d $IP_DMZ_SERVER -p tcp --sport 1024:65535 --dport 10090:10100 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+
 iptables -A FORWARD -i $IFACE_DMZ -s $IP_DMZ_SERVER -o $IFACE_WAN -d $IP_INET -p tcp --sport 10090:10100 --dport 1024:65535 -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 
